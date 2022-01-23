c982159f8551a1c9d1e40bf02f01b20cdbc26414
Jose Luis Chiquete <josecch@gmail.com>
2022-01-22 20:19:01 -0600

    220122a

--- src/fast_api/main.py
+++ src/fast_api/main.py
@@ -1,6 +1,6 @@
 from typing import List
 from fastapi import FastAPI, Depends
-from fastapi.exceptions import HTTPException, HTTPException
+from fastapi.exceptions import HTTPException
 from sqlalchemy.orm import Session
 import crud
 import models
